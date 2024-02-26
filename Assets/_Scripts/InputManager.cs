a2752ed64c00267717b81faefecf411d20f0e620
Omer Faruk Ozer <ozeromerfaruk@gmail.com>
2024-02-26 19:05:41 +0300

    create building

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -6,8 +6,10 @@ namespace _Scripts
     public class InputManager : MonoBehaviour
     {
         [SerializeField] private LayerMask _mouseInputLayerMask;
-        
-        
+        [SerializeField] private GameObject _buildPrefab;
+
+        private const int CELL_SIZE = 2;
+
         private void Update()
         {
             // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
@@ -15,6 +17,7 @@ namespace _Scripts
         }
         
         
+        // ReSharper disable Unity.PerformanceAnalysis
         private void GetInput()
         {
             if (!Input.GetMouseButtonDown(0) || EventSystem.current.IsPointerOverGameObject())
@@ -26,7 +29,22 @@ namespace _Scripts
                     _mouseInputLayerMask)) return;
             
             Vector3 position = hit.point - transform.position;
-            Debug.Log("Mouse position: " + position);
+            Debug.Log("Mouse position: " + CalculateGridPosition(position));
+            
+            Build(CalculateGridPosition(position));
+        }
+        
+        private Vector3 CalculateGridPosition(Vector3 inputPosition)
+        {
+            int x = Mathf.FloorToInt((float) inputPosition.x / CELL_SIZE);
+            int z = Mathf.FloorToInt((float) inputPosition.z / CELL_SIZE);
+            
+            return new Vector3(x * CELL_SIZE, 0, z * CELL_SIZE);
+        }
+        
+        private void Build(Vector3 position)
+        {
+            Instantiate(_buildPrefab, position, Quaternion.identity);
         }
     }
 }
