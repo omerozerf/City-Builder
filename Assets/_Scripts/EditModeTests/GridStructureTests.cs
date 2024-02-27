ae51881d6136979a13033bc8a2585f3ac33e55f5
Omer Faruk Ozer <ozeromerfaruk@gmail.com>
2024-02-27 11:41:37 +0300

    Update GridStructureTests.cs

--- Assets/_Scripts/EditModeTests/GridStructureTests.cs
+++ Assets/_Scripts/EditModeTests/GridStructureTests.cs
@@ -12,14 +12,6 @@ public class GridStructureTests
     {
         // Use the Assert class to test conditions
     }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator GridStructureTestsWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    }
+    
+    
 }
