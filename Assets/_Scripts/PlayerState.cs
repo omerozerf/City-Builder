5e4972d14e26f7dbf0098f003dc09952ce8d6851
Omer Faruk Ozer <ozeromerfaruk@gmail.com>
2024-02-29 17:32:12 +0300

    player state

---
+++ Assets/_Scripts/PlayerState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace _Scripts
+{
+    public abstract class PlayerState
+    {
+        private GameManager m_GameManager;
+        
+        
+        public PlayerState(GameManager gameManager)
+        {
+            m_GameManager = gameManager;
+        }
+        
+        
+        protected GameManager GetManager()
+        {
+            return m_GameManager;
+        }
+        
+        
+        public abstract void OnInputPointerDown(Vector3 position);
+        public abstract void OnInputPointerChange(Vector3 position);
+        public abstract void OnInputPointerUp();
+        public abstract void OnInputPanChange(Vector3 position);
+        public abstract void OnInputPanUp();
+
+    }
+}
