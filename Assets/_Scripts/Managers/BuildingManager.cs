6421016d5226237fc832576dfed8603c6d7c519d
Omer Faruk Ozer <ozeromerfaruk@gmail.com>
2024-03-08 14:59:32 +0300

    namespace

---
+++ Assets/_Scripts/Managers/BuildingManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Managers
+{
+    public class BuildingManager
+    {
+        private GridStructure m_GridStructure;
+        private PlacementManager m_PlacementManager;
+
+        public BuildingManager(PlacementManager placementManager, int cellSize, int width, int height)
+        {
+            m_PlacementManager = placementManager;
+        
+            m_GridStructure = new GridStructure(cellSize, width, height);
+        }
+
+        public void PlaceStructureAt(Vector3 position)
+        {
+            Vector3 gridPosition = m_GridStructure.CalculateGridPosition(position);
+        
+            if (!m_GridStructure.IsCellTaken(gridPosition))
+            {
+                m_PlacementManager.Build(gridPosition, m_GridStructure);
+            }
+        }
+
+        public void RemoveBuildingAt(Vector3 position)
+        {
+            Vector3 gridPosition = m_GridStructure.CalculateGridPosition(position);
+        
+            if (m_GridStructure.IsCellTaken(gridPosition))
+            {
+                m_PlacementManager.RemoveBuilding(gridPosition, m_GridStructure);
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/Managers/PlacementManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Managers
+{
+    public class PlacementManager : MonoBehaviour
+    { 
+        [SerializeField] private GameObject _buildPrefab;
+        [SerializeField] private Transform _groundTransform;
+
+
+        public void Build(Vector3 position, GridStructure gridStructure)
+        {
+            Vector3 fixedPosition = position + _groundTransform.position;
+            GameObject newStructure = Instantiate(_buildPrefab, fixedPosition, Quaternion.identity);
+            
+            gridStructure.PlaceStructureOnTheGrid(newStructure, fixedPosition);
+        }
+    
+        public void RemoveBuilding(Vector3 position, GridStructure gridStructure)
+        {
+            /*
+        Vector3 fixedPosition = position + _groundTransform.position;
+        gridStructure.RemoveStructureFromTheGrid(fixedPosition);
+        */
+        
+            GameObject structure = gridStructure.GetStructureFromTheGrid(position);
+
+            if (!structure) return;
+        
+            Destroy(structure);
+            gridStructure.RemoveStructureFromTheGrid(position);
+        }
+    }
+}
