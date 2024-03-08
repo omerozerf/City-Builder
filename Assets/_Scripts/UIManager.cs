f1d2bddec7771c094a5aa976e9e633ad5b052ccb
Omer Faruk Ozer <ozeromerfaruk@gmail.com>
2024-03-08 13:46:32 +0300

    recaftor ui

--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -10,6 +10,12 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject _buildingMenuPanel;
     [SerializeField] private Button _openBuildMenuButton;
     [SerializeField] private Button _demolishButton;
+    [SerializeField] private GameObject _zonesPanel;
+    [SerializeField] private GameObject _facilitiesPanel;
+    [SerializeField] private GameObject _roadsPanel;
+    [SerializeField] private Button _closeBuildMenuButton;
+    [SerializeField] private GameObject _buildButtonPrefab;
+    
 
     public event Action OnBuildResidentialAreaButtonClicked;
     public event Action OnCancelActionButtonClicked;
@@ -31,38 +37,47 @@ public class UIManager : MonoBehaviour
 
     private void InitializeListeners()
     {
-        _buildResidentialAreaButton.onClick.AddListener(OnBuildAreaCallback);
+        // _buildResidentialAreaButton.onClick.AddListener(OnBuildAreaCallback);
         _cancelActionButton.onClick.AddListener(OnCancelActionCallback);
         _openBuildMenuButton.onClick.AddListener(OnOpenBuildMenuCallback);
         _demolishButton.onClick.AddListener(OnDemolishButtonCallback);
+        _closeBuildMenuButton.onClick.AddListener(OnCloseMenuCallback);
     }
     
     private void RemoveListeners()
     {
-        _buildResidentialAreaButton.onClick.RemoveListener(OnBuildAreaCallback);
+        // _buildResidentialAreaButton.onClick.RemoveListener(OnBuildAreaCallback);
         _cancelActionButton.onClick.RemoveListener(OnCancelActionCallback);
         _openBuildMenuButton.onClick.RemoveListener(OnOpenBuildMenuCallback);
         _demolishButton.onClick.RemoveListener(OnDemolishButtonCallback);
+        _closeBuildMenuButton.onClick.RemoveListener(OnCloseMenuCallback);
     }
     
+    
+    private void OnCloseMenuCallback()
+    {
+        _buildingMenuPanel.SetActive(false);
+        
+    }
     
     private void OnDemolishButtonCallback()
     {
         OnDemolishButtonClicked?.Invoke();
         _cancelActionPanel.SetActive(true);
-        _buildingMenuPanel.SetActive(false);
+        OnCloseMenuCallback();
     }
 
     private void OnOpenBuildMenuCallback()
     {
         _buildingMenuPanel.SetActive(true);
+        
+        PrepareBuildMenu();
     }    
 
     private void OnBuildAreaCallback()
     {
         _cancelActionPanel.SetActive(true);
-        _buildingMenuPanel.SetActive(false);    
-        
+        OnCloseMenuCallback();   
         OnBuildResidentialAreaButtonClicked?.Invoke();
     }
         
@@ -71,5 +86,26 @@ public class UIManager : MonoBehaviour
         _cancelActionPanel.SetActive(false);
             
         OnCancelActionButtonClicked?.Invoke();
+    }
+
+
+    private void PrepareBuildMenu()
+    {
+        CreateButtonsInPanel(_zonesPanel.transform);
+        CreateButtonsInPanel(_facilitiesPanel.transform);
+        CreateButtonsInPanel(_roadsPanel.transform);
+    }
+
+    private void CreateButtonsInPanel(Transform panelTransform)
+    {
+        foreach (Transform childTransform in panelTransform)
+        {
+            Button button = childTransform.GetComponent<Button>();
+
+            if (button)
+            {
+                button.onClick.AddListener(OnBuildAreaCallback);
+            }
+        }
     }
 }
